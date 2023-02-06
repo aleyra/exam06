82608a0e6c01aec05b9a2979e1df7da3fed6972a
lburnet <lburnet@student.42lyon.fr>
2023-02-06 22:34:45 +0100

    il me manque pas grand chose

--- do_not_forget.c
+++ do_not_forget.c
@@ -1,4 +1,4 @@
-void	fatal_error(void){//<-
+void	fatal_error(void){//<-pas de param
 	//
 	close(sock_fd);//sock_fd car pas de fd ds param
 	//
@@ -8,14 +8,14 @@ int	rm_client(int fd){
 	if (t_cli->fd == fd){
 		//
 	} else {
-		while (t_cli && t_cli->next && t_cli->next->fd != fd)//<-
+		while (t_cli && t_cli->next && t_cli->next->fd != fd)//<-pour une fois on ne tourne pas jusqu'au bout
 			//
 		//
 	}
 	//
 }
 
-void	accept_client(void){//<-c'est le accept qui def le fd
+void	accept_client(void){//<-c'est le accept qui def le fd, pas de param
 	//
 	int					cli_fd;
 
@@ -26,15 +26,15 @@ void	accept_client(void){//<-c'est le accept qui def le fd
 
 void	send_all(int fd, char *ze_msg){
 	while (t_cli){
-		if (t_cli->fd != fd && FD_ISSET(t_cli->fd, &cpy_write))//<-
+		if (t_cli->fd != fd && FD_ISSET(t_cli->fd, &cpy_write))//<-penser au FD_ISSET
 			//
 		//
 	}
 }
 
-void	send_msg(int fd){//<-
+void	send_msg(int fd){//<-on appelle une var globale donc a ne pas mettre dans les params
 	//
-	bzero(&str, strlen(str));
+	bzero(&str, strlen(str));//ne pas oublier
 }
 
 //main
@@ -48,7 +48,7 @@ int	main(int ac, char** av){
 	while(1){
 		//
 		for (int fd = 0; fd <= get_max_fd(); fd++){
-			if (FD_ISSET(fd, &cpy_read)){//<-
+			if (FD_ISSET(fd, &cpy_read)){//<-FD_ISSET c'est pas avec &curr_sock
 				if (fd == sock_fd){
 					//
 					break ;
